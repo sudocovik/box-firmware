30d1fed603b38ca8fbbd0419484253f27b3970b6
covik <mate.nakic3@gmail.com>
2021-02-19 14:10:07 +0100

    Add support for serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);		// Initialize serial communications with the PC
+    while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+
+    Serial.println("Hi!");
 }
 
 void loop() {
-// write your code here
+
 }
