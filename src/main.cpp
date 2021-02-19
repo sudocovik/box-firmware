b4f9ca89ecb53946689c9e370ae2b32f52d75b49
covik <mate.nakic3@gmail.com>
2021-02-19 19:39:34 +0100

    Add basic UID reading from code examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN     D1
+#define SS_PIN      D2
+
+MFRC522 mfrc522 = MFRC522(SS_PIN, RST_PIN);
 
 void setup() {
-    Serial.begin(9600);		// Initialize serial communications with the PC
-    while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    Serial.begin(9600);
+    while (!Serial);
 
-    Serial.println("Hi!");
+    SPI.begin();
+    mfrc522.PCD_Init();
+    delay(4);
+
+    mfrc522.PCD_DumpVersionToSerial();
 }
 
 void loop() {
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        return;
+    }
 
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+        Serial.println("Bad read (was card removed too quickly?)");
+        return;
+    }
+
+    if (mfrc522.uid.size == 0) {
+        Serial.println("Bad card (size = 0)");
+    } else {
+        char tag[sizeof(mfrc522.uid.uidByte) * 4] = { 0 };
+        for (int i = 0; i < mfrc522.uid.size; i++) {
+            char buff[5]; // 3 digits, dash and \0.
+            snprintf(buff, sizeof(buff), "%s%d", i ? "-" : "", mfrc522.uid.uidByte[i]);
+            strncat(tag, buff, sizeof(tag));
+        };
+        Serial.println("Good scan: ");
+        Serial.println(tag);
+    };
+
+    // disengage with the card.
+    mfrc522.PICC_HaltA();
 }
