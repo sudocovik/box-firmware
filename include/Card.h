a35b54b4dd003365a1f8566b0b6bfa233aee5b4d
covik <mate.nakic3@gmail.com>
2021-02-27 16:46:59 +0100

    Convert passed Uid object to string representation

--- include/Card.h
+++ include/Card.h
@@ -1,8 +1,16 @@
 #ifndef CARD_H
 #define CARD_H
 
-class Card {
+#include <MFRC522.h>
 
+class Card {
+    private:
+        String UID = "";
+
+        static String uidToHexString(MFRC522::Uid uid);
+
+    public:
+        explicit Card(MFRC522::Uid uid);
 };
 
 #endif
--- src/Card.cpp
+++ src/Card.cpp
@@ -1,1 +1,22 @@
 #include <Card.h>
+#include <MFRC522.h>
+
+Card::Card(MFRC522::Uid uid) {
+    UID = uidToHexString(uid);
+}
+
+String Card::uidToHexString(MFRC522::Uid uid) {
+    String hexString = "";
+
+    for(unsigned short int i = 0; i < uid.size; i++) {
+        const char prefix = uid.uidByte[i] < 10 ? '0' : '\0';
+
+        String byteAsHexString = prefix + String(uid.uidByte[i], HEX);
+        hexString += byteAsHexString + " ";
+    }
+
+    hexString.trim();
+    hexString.toUpperCase();
+
+    return hexString;
+}
